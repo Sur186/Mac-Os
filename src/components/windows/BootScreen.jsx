a8cff451dece4cf98ec8cd5a3dc3668968ee2770
Surendra Puri <surendrapuri5121@gmail.com>
2026-08-10 18:33:46 +0530

    Finalize portfolio

--- src/components/windows/BootScreen.jsx
+++ src/components/windows/BootScreen.jsx
@@ -9,7 +9,7 @@ const BootScreen = () => {
           alt="DeveloperOS Logo"
         />
         <h1>Dev Puri OS</h1>
-        <p>Welcome to Dev Puri OS — Live 🚀</p>
+        <p>Interactive React Portfolio...</p>
         <div className="loader">
           <div className="loader-fill"></div>
         </div>
